a4c9b0fff409bb849b2f1191e3c000e6b7b2100b
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2025-10-31 10:34:58 +0600

    add

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const testomonialRoute = require("./routes/testomonialRoute");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:3000","http://localhost:3001","http://localhost:3002","https://mhshishir.vercel.app","https://adminmhshishir.vercel.app"],
+  origin: ["http://localhost:3000","http://localhost:3001","http://localhost:3002","https://mhshishir.vercel.app","https://adminmhshishir.vercel.app","https://mehedi-hasan-shishir.vercel.app"],
   credentials: true,
 }));
 
